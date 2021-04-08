b420219bbee65162efeb1dfd8aee6c76560f7c48
karin valcanover <valcanoverkarin@outlook.it>
2021-04-08 15:42:40 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 var rightChevron = $('.chevron.right');
 
 rightChevron.click(function() {
+    //alert('test')
+
     //1.capire chi ha la classe active
     var currentActive = $('.slider .active');
     console.log(currentActive);
@@ -13,7 +15,7 @@ rightChevron.click(function() {
         //6.aggiungo la classe active alla prima immagine
         $('.slider .first').addClass('active');
     } else {
-        //7.aggiungo la classe active all'elemento successivo 
+        //7.altrimenti aggiungo la classe active all'elemento successivo 
         var nextImg = currentActive.next();
         nextImg.addClass('active');
     }
@@ -30,5 +32,20 @@ rightChevron.click(function() {
 var leftChevron = $('.chevron.left')
 
 leftChevron.click(function() {
-    
-}
+    //alert('test')
+
+    //8.prendo l elemento attivo corrente
+    var currentActive = $('.slider .active');
+    //9.tolgo la classe active
+    currentActive.removeClass('active');
+
+    //10.se l' elemento attivo ha classe first(primo)
+    if( currentActive.hasClass('first')){
+        //11.aggiungo la classe active all' ultima immagine che ha classe last
+        $('.slider .last').addClass('active');
+    } else {
+        //12.altrimenti aggiungo la classe active all'elemento precedenti
+        var prevImg = currentActive.prev();
+        prevImg.addClass('active');
+    }
+});
