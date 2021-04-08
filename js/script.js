1f710761e4197e33f13947125d1726208051b98b
karin valcanover <valcanoverkarin@outlook.it>
2021-04-08 15:11:50 +0200

    classe active, tolgo classe active, seleziono elemento sucessivo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var rightChevron = $('.chevron.right');
+
+rightChevron.click(function() {
+    //capire chi ha la classe active
+    var currentActive = $('.slider .active');
+    console.log(currentActive);
+    //tolgo la classe active
+    currentActive.removeClass('active');
+    
+    //devo selezionare l elemento successivo avendo l elemento corrente
+    var nextImg = currentActive.next();
+    console.log(nextImg)
+});
